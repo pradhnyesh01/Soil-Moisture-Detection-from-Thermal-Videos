e88fd8717373f602377a8bf47a1d7017f89c61fb
Pradhnyesh Vijay Khorgade <pradhnyesh@Pradhnyeshs-MacBook-Air.local>
2025-10-31 14:00:40 +0530

    Minor changes

--- scripts/run_processing.py
+++ scripts/run_processing.py
@@ -3,7 +3,7 @@ from src.data_processing import process_dataset_folder
 
 if __name__ == "__main__":
     base_folder = '/Users/pradhnyesh/Documents/dataset'
-    frames_save_root = 'saved_frames1'
+    frames_save_root = 'saved_frames'
     output_csv = 'all_frame_features.csv'
 
     dataset_df = process_dataset_folder(base_folder, frames_save_root, output_csv)
